920b7759327c4d58f7a72fcc11459795d1a510aa
Ulyss Trachet <ulysstrachet@gmail.com>
2021-10-18 15:51:16 +0200

    Initialisation et affichage du tableau P4

--- puis4.c
+++ puis4.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
-int main(void) {
-  int x,y;
+#define NBL 6
+#define NBC 7
 
-for (x=0; x<7; x++)
-{
-    printf (" +");
-    for (y=0; y<7; y++)
-      printf("---+");
-    printf ("\n%d", x+1);
-    printf (" |");
-    for (y=0; y<7; y++)
-      printf(" |");
-    printf ("\n");
+char tab[NBL][NBC];		//Défini les valeurs du tableau.
+
+void tab_init(void) {		//Mise en forme du tableau.
+  for(int y=0;y<NBL;y++)
+     for(int x=0;x<NBC;x++)
+	tab[y][x] = '.';
 }
-printf (" +");
-for (y=0; y<7; y++)
-  for (y=0; y<7; y++)
-    printf("---+");
-printf ("\n");
+
+void print_tab(void) {		//Appel du tableau.
+  printf("-------------\n");	//Haut du tableau.
+  for(int y=0;y<NBL;y++) {
+    for(int x=0;x<NBC;x++) {
+       char case_tab = tab[y][x];
+	printf("%c", case_tab);	//Afficher une variable en tant que caractère.
+	printf(" ");
+    }
+    printf("\n");		//Retour à la ligne.
+  }
+  printf("-------------\n");	//Bas du tableau.
+  printf("1 2 3 4 5 6 7\n");
+}
+
+int main(void) {		//Début de l'affichage du code.
+  tab_init();
+  print_tab();
 }
